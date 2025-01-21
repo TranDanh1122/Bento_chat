3e53483e3ccda6a1ab659f2a3417d143b8344392
TranDanh1122 <trandanh14042000@gmail.com>
2025-01-21 16:38:00 +0700

    update notification layout

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,20 +1,21 @@
 import clsx from "clsx";
 import React from "react";
 import { NavLink } from "react-router-dom";
-const User = React.memo(({ user, children, isOpen }: { user: User, children: React.ReactNode, isOpen?: boolean }): React.JSX.Element => {
+const User = React.memo(({ user, children, isOpen, isHideActive }: { user: User, children: React.ReactNode, isOpen?: boolean, isHideActive?: boolean }): React.JSX.Element => {
     return <div className="flex items-center justify-start gap-4 relative">
-        <span className={clsx("w-3 h-3 rounded-full top-[10%] left-0 absolute", {
+        {!isHideActive && <span className={clsx("w-3 h-3 rounded-full top-[10%] left-0 absolute", {
             "bg-slate-400": user.status != "active",
             "bg-green-400": user.status == "active"
         })}></span>
+        }
 
         <img src={user.avatar ?? "/images/avt_5.png"} alt="" className="w-8 h-8 object-cover rounded-full" />
         {(isOpen || isOpen == undefined) && <div className="flex flex-col gap-1">
-            <NavLink to="#" className="base-14_sm text-bsColor/70 text-opacity-80 text-nowrap text-ellipsis">{`${user.firstName} ${user.lastName}`}</NavLink>
-            <span className="text-bsColor/50 text-opacity-45 caption">@kohaku</span>
+            <NavLink to="#" className="base-14_sm text-bsColor/95 text-opacity-80 text-nowrap text-ellipsis">{`${user.firstName} ${user.lastName}`}</NavLink>
+            <span className="text-bsColor/50 text-opacity-45 caption">{user.cover}</span>
         </div>}
         {children}
-    </div>
+    </div >
 })
 User.displayName = "User"
 export default User
--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -1,14 +1,82 @@
 import React from "react";
-import Sidebar from "../Layout/Sidebar";
-import RightBar from "../Layout/RightBar";
+import Layout from "../Layout/Layout";
+import ButtonGroup from "../components/ButtonGroup";
+import User from "../components/User";
+import clsx from "clsx";
 export default function Notification(): React.JSX.Element {
     return (
-        <div className="container mb:max-w-none w-full h-full min-h-[100vh] flex rounded-[3rem] " >
-            <Sidebar />
-            <main className="w-auto min-w-[58%] h-full min-h-full bg-bsColor/15 p-3 backdrop-blur-xl  overflow-hidden">
-
-            </main >
-            <RightBar />
-        </div >
+        <Layout>
+            <div className="flex items-center  justify-between mb-4">
+                <h1 className="title text-bsColor/95">Notifications</h1>
+                <div className="w-11 h-11 flex justify-center items-center bg-[#383838] rounded-full cursor-pointer">
+                    <img src="/images/setting.svg" className="w-6 h-6 object-cover" />
+                </div>
+            </div>
+            <ButtonGroup buttons={[
+                { title: "All", handleClick: () => { }, default: true },
+                { title: "Liked", handleClick: () => { } },
+                { title: "Replies", handleClick: () => { } },
+                { title: "Follows", handleClick: () => { } }
+            ]}></ButtonGroup>
+            <div className="flex flex-col gap-2 py-4 overflow-y-scroll scrollbar-none max-h-[calc(100vh-100px)]">
+                <Noti title="Kohaku, Moyo Shiro, Dash and 2 others" type="follow" desc="follow you" />
+                <Noti title="Kohaku liked your post" desc="SimpleList link.com/media.png" type="liked" />
+                <Noti title="Kohaku reposted your post" type="repost" desc="SimpleList link.com/media.png" />
+                <Noti title="Kohaku commented on your post" type="comment" desc="Always amazed with how you present your work. Love it so much" />
+                <Noti title="Kohaku, Moyo Shiro, Dash and 2 others" type="follow" desc="follow you" />
+                <Noti title="Kohaku liked your post" desc="SimpleList link.com/media.png" type="liked" />
+                <Noti title="Kohaku reposted your post" type="repost" desc="SimpleList link.com/media.png" />
+                <Noti title="Kohaku commented on your post" type="comment" desc="Always amazed with how you present your work. Love it so much" />
+                <Noti title="Kohaku, Moyo Shiro, Dash and 2 others" type="follow" desc="follow you" />
+                <Noti title="Kohaku liked your post" desc="SimpleList link.com/media.png" type="liked" />
+                <Noti title="Kohaku reposted your post" type="repost" desc="SimpleList link.com/media.png" />
+                <Noti title="Kohaku commented on your post" type="comment" desc="Always amazed with how you present your work. Love it so much" />
+                <Noti title="Kohaku, Moyo Shiro, Dash and 2 others" type="follow" desc="follow you" />
+                <Noti title="Kohaku liked your post" desc="SimpleList link.com/media.png" type="liked" />
+                <Noti title="Kohaku reposted your post" type="repost" desc="SimpleList link.com/media.png" />
+                <Noti title="Kohaku commented on your post" type="comment" desc="Always amazed with how you present your work. Love it so much" />
+                <Noti title="Kohaku, Moyo Shiro, Dash and 2 others" type="follow" desc="follow you" />
+                <Noti title="Kohaku liked your post" desc="SimpleList link.com/media.png" type="liked" />
+                <Noti title="Kohaku reposted your post" type="repost" desc="SimpleList link.com/media.png" />
+                <Noti title="Kohaku commented on your post" type="comment" desc="Always amazed with how you present your work. Love it so much" />
+                <Noti title="Kohaku, Moyo Shiro, Dash and 2 others" type="follow" desc="follow you" />
+                <Noti title="Kohaku liked your post" desc="SimpleList link.com/media.png" type="liked" />
+                <Noti title="Kohaku reposted your post" type="repost" desc="SimpleList link.com/media.png" />
+                <Noti title="Kohaku commented on your post" type="comment" desc="Always amazed with how you present your work. Love it so much" />
+            </div>
+        </Layout >
     )
+}
+export function Noti({ title, desc, type }: { title: string, desc: string, type: string }): React.JSX.Element {
+    const data = {} as User
+    const ref = React.useRef<HTMLDivElement>(null)
+    React.useEffect(() => {
+        if (ref.current) {
+            ref.current.querySelector("img")?.classList.remove("w-8", "h-8")
+            ref.current.querySelector("img")?.classList.add("w-11", "h-11")
+        }
+    }, [])
+    // const constent = {
+    //     follow: "+",
+    //     liked: "",
+    //     repost: "repost your post",
+    //     comment: "comment on your post"
+    // }
+    return (<>
+        <div ref={ref} className="bg-bsColor/5 p-3 rounded-2xl" >
+            <User user={{ ...data, firstName: title, cover: desc }} isHideActive={true} >
+                <span className={clsx("w-5 h-5 rounded-full top-10 left-6 absolute z-20 flex items-center justify-center", {
+                    "bg-purple-500": type == "follow",
+                    "bg-red-500": type == "liked",
+                    "bg-green-500": type == "repost",
+                    "bg-blue-500": type == "comment",
+                })}>
+                    +
+                </span>
+            </User>
+            <span className="caption_r text-bsColor/50 text-opacity-50 ml-16 mt-3">3h ago</span>
+        </div>
+
+
+    </>)
 }
